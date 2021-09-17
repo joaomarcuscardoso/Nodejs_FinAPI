377f643ba8e261a5ab309af146c59b4b19aee781
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-09-16 22:33:23 -0300

    check the user's CPF

--- src/index.js
+++ src/index.js
@@ -8,15 +8,21 @@ app.use(express.json())
 const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4()
     /**
-    * id - uuid
-    * statement[] -> extrato
-    */
+     * id - uuid
+     * statement[] -> extrato
+     */
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already Exists!" });
+    }
+
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
